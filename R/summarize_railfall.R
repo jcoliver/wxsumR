df8dcda6c43425bc85ed82293c44e0f0539cc8bc
Jeff Oliver <jcoliver@email.arizona.edu>
2019-11-17 09:44:10 -0600

    Add time profiling to enumerate tests

--- R/summarize_railfall.R
+++ R/summarize_railfall.R
@@ -1,44 +1,49 @@
+# TODO: Given the wrapper nature of this function, would be good to add some
+# defensive programming here
+# File exists
+# No February 29, or at least a warning
+
+# TODO: Add parameter for site id (could assume column 1)
+
 #' Provides rainfall summary statistics
 #'
-#' @param inputfile   path to csv file with daily rainfall measurement
-#' @param outputfile  path to output file
+#' @param inputfile    path to csv file with daily rainfall measurement
+#' @param outputfile   path to output file
 #' @param start_month  numeric starting month defining season (inclusive)
 #' @param end_month    numeric ending month defining season (inclusive)
-#' @param day          numeric day of month defining season (inclusive); 
-#' 
-#' @return NULL if outputfile is given, if outputfile is NULL, returns data 
+#' @param day          numeric day of month defining season (inclusive);
+#'
+#' @return NULL if outputfile is given, if outputfile is NULL, returns data
 #' frame with rainfall summary statistics
 #' @import tidyverse
-summarize_rainfall <- function(inputfile, start_month, end_month, day = 15, 
+summarize_rainfall <- function(inputfile, start_month, end_month, day = 15,
                                outputfile = "results_rain.csv", na.rm = TRUE) {
-  # TODO: Given the wrapper nature of this function, would be good to add some 
-  # defensive programming here
-  # File exists
-  # No February 29, or at least a warning
-  
   # Read in the data
   rain <- read.csv(file = inputfile)
-  
+
   # Use to_long to convert to long format and parse column names into dates
   rain_long <- to_long(data = rain)
-  
+
   # Enumerate seasons
-  rain_long <- enumerate_seasons(data = rain_long, 
+  rain_long <- enumerate_seasons(data = rain_long,
                                  start_month = start_month,
                                  end_month = end_month,
                                  day = day)
-  
+
+  # Assume first column has site id
+
+
   # Do summary calculations for each season
   #    mean
   #    median
   #    sd
   #    total
-  #    skew: (mean - median)/sd 
+  #    skew: (mean - median)/sd
   #    days with < 1mm
   #    days with >= 1mm
   #    % days with >= 1mm
   #    longest consecutive period in season with < 1mm
-  
+
   # TODO: group_by location, too?
   rain_summary <- rain_long %>%
     group_by(season_year) %>%
@@ -50,6 +55,6 @@ summarize_rainfall <- function(inputfile, start_month, end_month, day = 15,
               norain = sum(x = value < 1, na.rm = na.rm),
               raindays = sum(x = value >= 1, na.rm = na.rm),
               raindays_percent = sum(x = value >= 1, na.rm = na.rm)/n())
-  
+
   return(rain_summary)
 }
--- examples/test-enumerate_seasons.R
+++ examples/test-enumerate_seasons.R
@@ -16,30 +16,44 @@ long_df <- to_long(data = df)
 # Exclude NA dates
 long_df <- long_df[!is.na(long_df$date), ]
 
-# Test 1 for season that includes new year
+# Test 1, season includes new year
+test_num <- 1
+message(paste0("Running test ", test_num, " of enumerate_seasons"))
 start_month <- 11
 end_month <- 02
 day <- 15
+test_start <- Sys.time()
 enumerated_df <- enumerate_seasons(data = long_df,
                                    start_month = start_month,
                                    end_month = end_month,
                                    day = day)
-if (all(table(enumerated_df$season_year)) == 9300) {
-  message("Test 1 PASS")
+test_end <- Sys.time()
+if (all(table(enumerated_df$season_year) == 9300)) {
+  message("Test ", test_num, " PASS")
 } else {
-  message("Test 1 FAIL")
+  message("Test ", test_num, " FAIL")
 }
+test_time <- difftime(time1 = test_end, time2 = test_start, units = "mins")
+test_time <- round(test_time, digits = 3)
+message(paste0("Test ", test_num, " time: ", test_time, " minutes"))
 
-# Test 1 for season that includes new year
+# Test 2, season excludes new year
+test_num <- 2
+message(paste0("Running test ", test_num, " of enumerate_seasons"))
 start_month <- 02
 end_month <- 11
 day <- 15
+test_start <- Sys.time()
 enumerated_df <- enumerate_seasons(data = long_df,
                                    start_month = start_month,
                                    end_month = end_month,
                                    day = day)
+test_end <- Sys.time()
 if (all(table(enumerated_df$season_year) == c(27400, 27500, 27400, 27400, 27400, 12800))) {
-  message("Test 1 PASS")
+  message("Test ", test_num, " PASS")
 } else {
-  message("Test 1 FAIL")
+  message("Test ", test_num, " FAIL")
 }
+test_time <- difftime(time1 = test_end, time2 = test_start, units = "mins")
+test_time <- round(test_time, digits = 3)
+message(paste0("Test ", test_num, " time: ", test_time, " minutes"))
