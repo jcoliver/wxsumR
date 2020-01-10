ffd01bf0496eb4d8362b9ac1b14f656ee7536659
jcoliver <jcoliver@email.arizona.edu>
2020-01-09 17:14:56 -0700

    Add check for long NA stretches in dry_interval

--- R/dry_interval.R
+++ R/dry_interval.R
@@ -21,13 +21,17 @@
 #'   will be zero}
 #' }
 #'
-#' @importFrom stringr str_split str_c
+#' @importFrom stringr str_split str_c str_sub
 dry_interval <- function(x, rain_cutoff = 1, period = c("start", "mid", "end")) {
   # A string that is a concatenation of 0s and 1s, where 0s are days where
   # rainfall is below rain_cutoff and 1s are days where rain is above
   # rain_cutoff
   rain_string <- stringr::str_c(as.integer(x >= rain_cutoff), collapse = "")
   
+  if (length(rain_string) == 0) {
+    return(NA)
+  }
+
   # Split the string into a vector using 1 as delimiter; results in vector that
   # has empty character strings (previously had values of 1) and strings of
   # some number of consecutive 0s. Using stringr::str_split instead of
@@ -40,7 +44,10 @@ dry_interval <- function(x, rain_cutoff = 1, period = c("start", "mid", "end")) 
   
   longest <- 0
   if (period == "start") {
-    longest <- nchar(rain_string_split)[1]
+    # Only return non-zero value for longest at start if first day was not dry
+    if (stringr::str_sub(string = rain_string, start = 1, end = 1) == 1) {
+      longest <- nchar(rain_string_split)[1]
+    }
   } else if (period == "mid") {
     # If entire season was rain or non-rain, or if the season was characterized
     # by a single stretch of rain followed by consecutive non-rain days (or
@@ -49,8 +56,11 @@ dry_interval <- function(x, rain_cutoff = 1, period = c("start", "mid", "end")) 
       longest <- max(nchar(rain_string_split[-c(1, length(rain_string_split))]))
     }
   } else if (period == "end") {
-    longest <- nchar(rain_string_split)[length(rain_string_split)]
+    # Only return non-zero value for longest at start if last day was not dry
+    if (stringr::str_sub(string = rain_string, start = nchar(rain_string), end = nchar(rain_string)) == 1) {
+      longest <- nchar(rain_string_split)[length(rain_string_split)]
+    }
   }
-  
+
   return(longest)
 }
