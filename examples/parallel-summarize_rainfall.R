86268ce14bc0398cf25e4795b3cb397cf687b574
jcoliver <jcoliver@email.arizona.edu>
2020-01-23 17:12:08 -0700

    Add comments about memory leak

--- examples/parallel-summarize_rainfall.R
+++ examples/parallel-summarize_rainfall.R
@@ -15,18 +15,30 @@ rm(list = ls())
 # With large data set, the parallel approach takes longer (203 vs. 62 seconds);
 # even just the parLapply execution takes a substantial amount of time (153) 
 # seconds.
+
 # There is still a MASSIVE memory leak. After running this with the large data
 # and clearing the environment with rm(list = ls()), the rsession was still 
-# holding onto 3.4 GB of RAM. A call to gc freed up a little bit (0.4 GB), but 
+# holding onto 3.4 GB of RAM. A call to gc() freed up a little bit (0.4 GB), but 
 # still not enough.
+
 # Try the .ls.objects function at https://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
-# to see if there is something hanging around. Especially after an rm() call.
+# to see if there is something hanging around. Especially after an rm() call. 
+
+# See also https://cran.r-project.org/web/packages/profmem/vignettes/profmem.html
+# for using profmem
+
+# Also in the thread of the above SO topic is this quote: 
+#   "If you really want to avoid the leaks, you should avoid creating any big 
+#   objects in the global environment. What I usually do is to have a function 
+#   that does the job and returns NULL — all data is read and manipulated in 
+#   this function or others that it calls."
+
 library(weathercommand)
 library(parallel)
 
 # infile <- "data/input-rain-small.csv"
-# infile <- "data/input-rain-medium.csv"
-infile <- "data/input-rain-large.csv"
+infile <- "data/input-rain-medium.csv"
+# infile <- "data/input-rain-large.csv"
 
 test_data <- read.csv(file = infile)
 
@@ -47,11 +59,11 @@ clusterEvalQ(clust, library(weathercommand))
 par_start <- Sys.time()
 
 # Have to make this a list of one-row data frames before sending to lapply
-test_list <- split(x = test_data, f = seq(nrow(test_data)), drop = TRUE)
+# test_list <- split(x = test_data, f = seq(nrow(test_data)), drop = TRUE)
 # TODO: Look at dplyr::group_split, too
-# test_list <- test_data %>%
-#   dplyr::group_by(y4_hhid) %>%
-#   dplyr::group_split()
+test_list <- test_data %>%
+  dplyr::group_by(y4_hhid) %>%
+  dplyr::group_split()
 
 par_sum_start <- Sys.time()
 par_summary <- parLapply(cl = clust,
@@ -92,4 +104,4 @@ par_sum_time <- difftime(time1 = par_sum_end, time2 = par_sum_start, units = "se
 par_sum_time <- round(x = par_sum_time, digits = 3)
 message(paste0("Time for parLapply call: ", par_sum_time, " seconds"))
 message(paste0("Additional parallel processing time (incl. split call): ", 
-               (par_time - par_sum_time), " seconds"))
+               round(par_time - par_sum_time, digits = 3), " seconds"))
