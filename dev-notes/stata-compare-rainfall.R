9e89bdffc39816fc411a51c18d35c97db6528367
Jeff Oliver <jcoliver@email.arizona.edu>
2020-04-20 17:39:31 -0700

    Work comparing r and stata rain results

--- dev-notes/stata-compare-rainfall.R
+++ dev-notes/stata-compare-rainfall.R
@@ -12,7 +12,7 @@ r_results_file <- "data/r-rain-output.csv"
 stata_results_file <- "data/stata-rain-output.csv"
 
 # Differences above this warrant investigation
-delta_cutoff <- 1e-05
+delta_cutoff <- 5e-05
 
 r_results <- read_csv(r_results_file)
 stata_results <- read_csv(stata_results_file)
@@ -25,44 +25,54 @@ dplyr::setdiff(colnames(r_results), colnames(stata_results))
 # Remove those columns, as we cannot compare them
 r_results <- r_results %>%
   select(-starts_with(match = "dry_start_"), -starts_with("dry_end_"))
+# Make sure set members are identical
 dplyr::setdiff(colnames(r_results), colnames(stata_results))
+# Are columns in same order (if > 0, then no)
+sum(colnames(r_results) != colnames(stata_results))
+
+# Even though data sets have same column names, orders of columns are different
+# Rearrange columns so order is the same
+stata_results <- stata_results[, colnames(r_results)]
+# Are columns in same order (if > 0, then no)
+sum(colnames(r_results) != colnames(stata_results))
 
 # There is a good chance we have differences due to rounding, so create a
 # matrix of differences and see which columns have "big" deltas
-result_diffs <- r_results[, 2:ncol(r_results)] -
-  stata_results[, 2:ncol(stata_results)]
+result_diffs <- r_results[, 2:ncol(r_results)] - stata_results[, 2:ncol(stata_results)]
+result_diffs <- as.matrix(result_diffs)
 rownames(result_diffs) <- r_results$y4_hhid
 
-# This comparison isn't working; returning TRUE when it should not
-big_diffs <- result_diffs > delta_cutoff
-cols_with_big_diff <- colSums(big_diffs)
+stata_results$dev_raindays_1983[15:18]
 
-# Matrix conversion isn't working
-# See which columns have "big" delta
-result_diffs_mat <- as.matrix(result_diffs)
-big_diffs_mat <- result_diffs_mat > delta_cutoff
-cols_with_big_diff <- colSums(big_diffs_mat)
-# Idiosyncratic discrepancies by year. 1983 mean_season was identical between
-# R and stata results, but for 1985, mean_season was greater than delta_cutoff
-# all all 100 sites
-r_results$mean_season_1985[1:10]
-stata_results$mean_season_1985[1:10]
+# Where are differences "large"
+big_diffs <- abs(result_diffs) > delta_cutoff
+cols_with_big_diff <- colSums(big_diffs)[colSums(big_diffs) > 0]
+# dev_raindays_1983    dev_raindays_1984    dev_raindays_1985    dev_raindays_1986
+# 100                  100                  100                  100
+# mean_period_raindays   sd_period_raindays
+# 100                  100
 
 
-diff_means <- colMeans(x = result_diffs)
-big_diffs <- diff_means[diff_means > delta_cutoff]
-values_to_check <- names(big_diffs)
-values_to_check <- gsub(pattern = "198[3456]", replacement = "YYYY",
-                        x = values_to_check)
-values_to_check <- unique(values_to_check)
-# [1] "sd_season_YYYY"               "total_season_YYYY"
-# [3] "norain_YYYY"                  "raindays_YYYY"
-# [5] "dry_YYYY"                     "mean_season_YYYY"
-# [7] "z_total_season_YYYY"          "median_season_YYYY"
-# [9] "dev_total_season_YYYY"        "dev_norain_YYYY"
-# [11] "mean_period_total_season"     "sd_period_total_season"
-# [13] "mean_period_norain"           "sd_period_norain"
-# [15] "mean_period_raindays"         "sd_period_raindays"
-# [17] "mean_period_raindays_percent" "sd_period_raindays_percent"
+# Comparing percentages, not differences
+rho_cutoff <- 1e-4 #(0.0001, or 0.01%)
 
-# Read in data and do manual calculation
+# Will divide by R results, but to avoid dividing my zero, change all zeros to
+# a very small number (for dividing)
+r_denominator <- as.matrix(r_results[, 2:ncol(r_results)])
+r_denominator[r_denominator == 0] <- 1e-16
+result_rhos <- (abs(r_results[, 2:ncol(r_results)] -
+                  stata_results[, 2:ncol(stata_results)])) / r_denominator
+result_rhos <- as.matrix(result_rhos)
+rownames(result_rhos) <- r_results$y4_hhid
+
+big_rhos <- result_rhos > rho_cutoff
+cols_with_big_rho <- colSums(big_rhos)[colSums(big_rhos) > 0]
+#    dev_raindays_1983    dev_raindays_1984    dev_raindays_1985    dev_raindays_1986
+#                   20                   40                   60                   60
+# mean_period_raindays
+#                  100
+# Are they really that different?
+ids <- names(which(big_rhos[, "dev_raindays_1983"]))
+r_results$dev_raindays_1983[r_results$y4_hhid %in% ids]
+stata_results$dev_raindays_1983[stata_results$y4_hhid %in% ids]
+
