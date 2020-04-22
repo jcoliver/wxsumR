24d36f0bca6b77c22b627ac8bc212c89b4486a1b
Jeff Oliver <jcoliver@email.arizona.edu>
2020-04-21 17:11:56 -0700

    Fix percentage-based results comparison

--- dev-notes/stata-compare-rainfall.R
+++ dev-notes/stata-compare-rainfall.R
@@ -47,32 +47,54 @@ stata_results$dev_raindays_1983[15:18]
 # Where are differences "large"
 big_diffs <- abs(result_diffs) > delta_cutoff
 cols_with_big_diff <- colSums(big_diffs)[colSums(big_diffs) > 0]
-# dev_raindays_1983    dev_raindays_1984    dev_raindays_1985    dev_raindays_1986
-# 100                  100                  100                  100
-# mean_period_raindays   sd_period_raindays
-# 100                  100
-
+# dev_raindays_1983    dev_raindays_1984    dev_raindays_1985
+# 100                  100                  100
+# dev_raindays_1986 mean_period_raindays   sd_period_raindays
+# 100                  100                  100
 
 # Comparing percentages, not differences
 rho_cutoff <- 1e-4 #(0.0001, or 0.01%)
 
-# Will divide by R results, but to avoid dividing my zero, change all zeros to
-# a very small number (for dividing)
-r_denominator <- as.matrix(r_results[, 2:ncol(r_results)])
+# Will divide by sum of results, but to avoid dividing my zero, change all
+# zeros to a very small number (for dividing)
+r_denominator <- as.matrix(r_results[, 2:ncol(r_results)] +
+                             stata_results[, 2:ncol(r_results)])
 r_denominator[r_denominator == 0] <- 1e-16
-result_rhos <- (abs(r_results[, 2:ncol(r_results)] -
-                  stata_results[, 2:ncol(stata_results)])) / r_denominator
+result_rhos <- abs((r_results[, 2:ncol(r_results)] -
+                  stata_results[, 2:ncol(stata_results)]) / r_denominator)
 result_rhos <- as.matrix(result_rhos)
 rownames(result_rhos) <- r_results$y4_hhid
 
 big_rhos <- result_rhos > rho_cutoff
 cols_with_big_rho <- colSums(big_rhos)[colSums(big_rhos) > 0]
-#    dev_raindays_1983    dev_raindays_1984    dev_raindays_1985    dev_raindays_1986
-#                   20                   40                   60                   60
-# mean_period_raindays
-#                  100
-# Are they really that different?
-ids <- names(which(big_rhos[, "dev_raindays_1983"]))
-r_results$dev_raindays_1983[r_results$y4_hhid %in% ids]
-stata_results$dev_raindays_1983[stata_results$y4_hhid %in% ids]
+# dev_raindays_1983    dev_raindays_1984    dev_raindays_1985
+#               100                  100                  100
+# dev_raindays_1986 mean_period_raindays   sd_period_raindays
+#               100                  100                  100
 
+# Same results, either way. Three differences:
+# dev_raindays (for each year)
+# mean_period_raindays
+# sd_period_raindays
+
+# dev_raindays_1983 (order of magnitude difference)
+r_results[1:6, c(1,13)]
+stata_results[1:6, c(1,13)]
+# calculating dev_raindays for a year via: raindays - mean_period_raindays
+# That is, the total number of raindays minus the mean number of raindays over
+# the period investigated
+
+input_data <- read.csv(file = "data/stata-rain.csv")
+# Extract only those columns of interest
+# March 15 through November 15 1983
+# rf19830315 through rf19831115
+season_1983 <- input_data[, c(1, 75:320)]
+cols <- grep(pattern = "1983", x = colnames(input_data), value = TRUE)
+
+# Threshold for rain is 1 (the default)
+rain_threshold <- 1
+raindays_1983 <- rowSums(season_1983[, 2:ncol(season_1983)] >= rain_threshold)
+
+raindays_1983 <- data.frame(y4_hhid = season_1983$y4_hhid,
+                            raindays_1983 = raindays_1983)
+head(raindays_1983)
