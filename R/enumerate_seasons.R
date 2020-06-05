ff633bda00dc9070e989eb23da8b20da0d091d67
Jeff Oliver <jcoliver@email.arizona.edu>
2020-06-05 15:18:20 -0700

    Document enumerate_seasons & to_long

--- R/enumerate_seasons.R
+++ R/enumerate_seasons.R
@@ -1,7 +1,14 @@
 #' Enumerate seasons in data frame
 #'
-#' @param data         long-format data frame with weather data; ideally output
-#' of \code{toLong}
+#' @description \code{enumerate_seasons} categorizes rows by an annual season
+#' defined by \code{start_*} and \code{end_*} parameters. The resulting object
+#' includes a \code{season_year} column indicating which season the date is
+#' considered part of. This is generally only used for internal data
+#' processing by \code{summarize_*} functions.
+#'
+#' @param data         long-format data frame with weather data including a
+#' column \code{date}; ideally the data object is the result of a call to
+#' \code{\link{to_long}}
 #' @param start_month  numeric starting month defining season (inclusive)
 #' @param end_month    numeric ending month defining season (inclusive)
 #' @param start_day    numeric day of starting month defining season
@@ -9,10 +16,28 @@
 #' @param end_day      numeric day of ending month defining season (inclusive);
 #' defaults to \code{start_day}
 #'
-#' @return data frame with new columns, "season" and "season_year"
+#' @return A copy of the passed \code{data} data frame with an additional
+#' column, \code{season_year} indicating the year of the season that the
+#' observation "belongs" to. For seasons that \emph{do not} cross the new year,
+#' this is simply the year the observation was made. For seasons that \emph{do}
+#' include the new year (e.g. a season starts in November and ends in the
+#' subsequent March), \code{season_year} is defined as the year in which the
+#' season starts. That is, for a season spanning November through March, the
+#' value of \code{season_year} for observations in January, February, and March
+#' is the prior year (e.g. \code{season_year} for an observation on 1983-02-01
+#' for a season spanning November through March would be 1984).
 #'
 #' @examples
 #' \donttest{
+#' df <- readRDS(file = "data/rain-small.Rds")
+#' # convert to long format and parse column names into dates
+#' rain_long <- to_long(data = df)
+#' # enumerate seasons, defined by 30 November through 15 March
+#' rain_long <- enumerate_seasons(data = df,
+#'                                start_month = 11,
+#'                                end_month = 3,
+#'                                start_day = 30,
+#'                                end_day = 15)
 #' }
 #'
 #' @importFrom tidyr drop_na
@@ -37,7 +62,6 @@ enumerate_seasons <- function(data, start_month, end_month, start_day = 15,
                                   start_day = start_day,
                                   end_day = end_day)
 
-  # data <- tidyr::drop_na(data)
   data <- tidyr::drop_na(data = data, season_year)
   return(data)
 }
@@ -52,13 +76,33 @@ enumerate_seasons <- function(data, start_month, end_month, start_day = 15,
 #' @param end_day      numeric day of ending month defining season (inclusive);
 #' defaults to \code{start_day}
 #'
-#' @return  integer vector of season year to which observation corresponds to
+#' @return  integer vector of season year to which observation corresponds to.
+#' For seasons that \emph{do not} cross the new year, this is simply the year
+#' the observation was made. For seasons that \emph{do} include the new year
+#' (e.g. a season starts in November and ends in the subsequent March),
+#' \code{season_year} is defined as the year in which the season starts. That
+#' is, for a season spanning November through March, the value of
+#' \code{season_year} for observations in January, February, and March is the
+#' prior year (e.g. \code{season_year} for an observation on 1983-02-01 for a
+#' season spanning November through March would be 1984).
+#'
+#' @examples
+#' \donttest{
+#' dates <- as.Date(x = c("1983-12-15", "1984-03-01", "1984-12-15", "1985-03-01"))
+#' # identify season year, where season is defined as 30 November through
+#' # 15 March
+#' sy <- season_year(x = dates,
+#'                   start_month = 11,
+#'                   end_month = 3,
+#'                   start_day = 30,
+#'                   end_day = 15)
+#' }
 #'
 #' @importFrom lubridate year as_date
 #' @importFrom stringr str_c
 season_year <- function(x, start_month, end_month, start_day = 15,
                         end_day = start_day) {
-  # Will hold return value
+  # Will hold return value (sy = season year)
   sy <- rep(NA, length(x))
 
   # The year of observation in x
--- R/to_long.R
+++ R/to_long.R
@@ -1,14 +1,42 @@
 #' Convert wide format weather data to long format
-#' 
+#'
+#' @description \code{to_long} transforms wide-formatted data, where each
+#' column respresents an observation for a specific day for a specific site, to
+#' wide-formatted data, where each row corresponds to an observation for a
+#' single day for a specific site.
+#'
 #' @param data       wide format data
-#' @param keep_cols  columns in \code{data} to retain in long format; all other 
-#' columns will be converted to key/value pairs in long format; defaults to 1
-#' @param date_sep   character used to delimit variable prefix from date in 
+#' @param keep_cols  columns in \code{data} to retain in long format; all other
+#' columns will be converted to key/value pairs in long format; defaults to 1.
+#' This should most likely correspond to the index of the column containing the
+#' site unique identifier
+#' @param date_sep   character used to delimit variable prefix from date in
 #' column names; defaults to underscore ("_")
-#' 
-#' @return data frame where each row corresponds to an observation for a single 
-#' day
-#' 
+#'
+#' @details \code{to_long} assumes \code{data} has a column indicating site
+#' unique identifier (i.e. a site ID column) and remaining columns are daily
+#' measurements of a variable in question (i.e rainfall, temperature).
+#' Remaining column names are assumed to have a prefix, a separator character,
+#' and date in YYYYMMDD format. For example, rainfall data column names would
+#' be something like "rf_19830325". Character strings occuring before
+#' delimiter are not used. The character string in column names corresponding
+#' to date are used to extract the date (YYYYMMDD).
+#'
+#' If \code{to_long} encounters invalid dates (e.g. 30 February 1984), such
+#' dates will be assigned a value of \code{NA} in resultant data frame and a
+#' warning will be thrown.
+#'
+#' @return data frame where each row corresponds to an observation for a single
+#' day. Measurements are in the column \code{value} and date is in the column
+#' \code{date}. Measurements retain the class of original data, and date is of
+#' class \code\link{[base]{Date}}.
+#'
+#' @examples
+#' \donttest{
+#' df <- readRDS(file = "data/rain-small.Rds")
+#' rain_long <- to_long(data = df)
+#' }
+#'
 #' @import tidyr
 #' @import dplyr
 #' @importFrom stringr str_c
@@ -16,15 +44,15 @@
 to_long <- function(data, keep_cols = 1, date_sep = "_") {
   # Store names of columns to retain in output
   keep_col_names <- colnames(data)[keep_cols]
-  
-  # Convert to long
+
   # TODO: Need to check for cases when keep_cols length !=1
-  # TODO: What if there is not date separator?
+  # TODO: What if there is no date separator?
+  # Convert to long
   long_data <- data %>%
-    tidyr::pivot_longer(names_to = "col_name", 
-                        values_to = "value", 
+    tidyr::pivot_longer(names_to = "col_name",
+                        values_to = "value",
                         -keep_cols)
-  
+
   # Parse key column into year, month, day
   long_data <- long_data %>%
     # Start by creating a new column called "date"
@@ -42,6 +70,6 @@ to_long <- function(data, keep_cols = 1, date_sep = "_") {
   if (any(is.na(long_data$date))) {
     warning("Some columns could not be parsed into dates; this can occur if text parsing results in impossible dates, such as 30 February 1983")
   }
-  
+
   return(long_data)
 }
