d4e99175b3da9916906a5a8a5df40f84308daa38
Jeff Oliver <jcoliver@email.arizona.edu>
2020-06-26 16:45:18 -0700

    Start work on quickstart vignette

--- R/data.R
+++ R/data.R
@@ -1,21 +1,48 @@
-#' A small example data set of daily rainfall measurements
+#' A tiny (two-year) example data set of daily rainfall measurements
 #'
-#' @format A data frame with 100 rows and 2,000 columns
+#' @format A data frame with 100 rows and 732 columns
 #' \describe{
 #'   \item{y4_hhid}{unique ID of site}
 #'   \item{rf_19830101}{rainfall for 1983-01-01}
 #'   \item{rf_19830102}{rainfall for 1983-01-02}
 #'   \item{...}{}
+#'   \item{rf_19841231}{rainfall for 1984-12-31}
+#' }
+"rain-2yr.csv"
+
+#' A small (four-year) example data set of daily rainfall measurements
+#'
+#' @format A data frame with 100 rows and 1,462 columns
+#' \describe{
+#'   \item{y4_hhid}{unique ID of site}
+#'   \item{rf_19830101}{rainfall for 1983-01-01}
+#'   \item{rf_19830102}{rainfall for 1983-01-02}
+#'   \item{...}{}
+#'   \item{rf_19861231}{rainfall for 1986-12-31}
 #' }
 "rain-small"
 
-#' A small example data set of daily temperature measurements
+#' A tiny (two-year) example data set of daily temperature measurements
 #'
-#' @format A data frame with 100 rows and 2,000 columns
+#' @format A data frame with 100 rows and 732 columns
 #' \describe{
 #'   \item{hhid}{unique ID of site}
 #'   \item{tmp_19830101}{temperature for 1983-01-01, in degrees Celsius}
 #'   \item{tmp_19830102}{temperature for 1983-01-02, in degrees Celsius}
 #'   \item{...}{}
+#'   \item{tmp_19841231}{temperature for 1984-12-31, in degrees Celsius}
 #' }
-"rain-small"
+"temperature-2yr.csv"
+
+#' A small (four-year) example data set of daily temperature measurements
+#'
+#' @format A data frame with 100 rows and 1,462 columns
+#' \describe{
+#'   \item{hhid}{unique ID of site}
+#'   \item{tmp_19830101}{temperature for 1983-01-01, in degrees Celsius}
+#'   \item{tmp_19830102}{temperature for 1983-01-02, in degrees Celsius}
+#'   \item{...}{}
+#'   \item{tmp_19861231}{temperature for 1986-12-31, in degrees Celsius}
+#'   \item{...}{}
+#' }
+"temperature-4yr"
--- dev-notes/testing-data-setup.R
+++ dev-notes/testing-data-setup.R
@@ -9,47 +9,75 @@ rm(list = ls())
 # RAIN
 large_data <- read.csv(file = "../large-data-files/NPSY4_x1_rf1_daily.csv")
 
-# Data for stata comparison, 100 rows, 1462 columns (four years)
-write.csv(x = large_data[1:100, 1:1462], file = "data/stata-rain.csv", row.names = FALSE)
+# Tiny data, 6 x 732 (two years); largely for documentation
+# Saving as text to include reading in text file in vignette
+write.csv(x = large_data[1:6, 1:732],
+          file = "data/rain-2yr.csv",
+          row.names = FALSE)
+
+# Small data, 100 x 1462 (four years); largely for documentation
+# This is what was used for comparison with Stata output
+saveRDS(large_data[1:100, 1:1462],
+        file = "data/rain-4yr.Rds")
 
 # Small data, 100 x 2000
-write.csv(x = large_data[1:100, 1:2000], file = "data/input-rain-small.csv", row.names = FALSE)
-saveRDS(large_data[1:100, 1:2000], file = "data/rain-small.Rds")
+write.csv(x = large_data[1:100, 1:2000],
+          file = "data/input-rain-small.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:100, 1:2000],
+        file = "data/rain-small.Rds")
 
 # Medium wide data; 100 x 10000; 5 times more columns than small data
-write.csv(x = large_data[1:100, 1:10000], file = "data/input-rain-medium-wider.csv", row.names = FALSE)
+write.csv(x = large_data[1:100, 1:10000],
+          file = "data/input-rain-medium-wider.csv",
+          row.names = FALSE)
 
 # Medium data, 1000 x 2000; 10 times more rows than small data
-write.csv(x = large_data[1:1000, 1:2000], file = "data/input-rain-medium.csv", row.names = FALSE)
-saveRDS(large_data[1:1000, 1:2000], file = "data/rain-medium.Rds")
+write.csv(x = large_data[1:1000, 1:2000],
+          file = "data/input-rain-medium.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:1000, 1:2000],
+        file = "data/rain-medium.Rds")
 
 # Large data; 1000 x 10000; 10 times more rows, 5 times more columns than small
-write.csv(x = large_data[1:1000, 1:10000], file = "data/input-rain-large.csv", row.names = FALSE)
-saveRDS(large_data[1:1000, 1:10000], file = "data/rain-large.Rds")
-
-# Tiny data, 20 x 1000
-write.csv(x = large_data[1:20, 1:1000], file = "data/input-rain-tiny.csv", row.names = FALSE)
-
-# Data set with impossible dates and incomplete sampling through year
-messy_data <- read.csv(file = "data/ihs4p_x0_rf1_daily.csv")
-
-write.csv(x = messy_data[1:100, 1:1000], file = "data/input-rain-messy.csv", row.names = FALSE)
+write.csv(x = large_data[1:1000, 1:10000],
+          file = "data/input-rain-large.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:1000, 1:10000],
+        file = "data/rain-large.Rds")
 
 ########################################
 # TEMPERATURE
 large_data <- read.csv(file = "../large-data-files/NPSY1_x1_t1_daily.csv")
 
-# Data for stata comparison, 100 rows, 1462 columns (four years)
-write.csv(x = large_data[1:100, 1:1462], file = "data/stata-temperature.csv", row.names = FALSE)
+# Tiny data, 6 x 732 (two years); largely for documentation
+# Saving as text to include reading in text file in vignette
+write.csv(x = large_data[1:6, 1:732],
+          file = "data/temperature-2yr.csv",
+          row.names = FALSE)
+
+# Small data, 100 x 1462 (four years); largely for documentation
+# This is what was used for comparison with Stata output
+saveRDS(large_data[1:100, 1:1462],
+        file = "data/temperature-4yr.Rds")
 
 # Small data, 100 x 2000
-write.csv(x = large_data[1:100, 1:2000], file = "data/input-temperature-small.csv", row.names = FALSE)
-saveRDS(large_data[1:100, 1:2000], file = "data/temperature-small.Rds")
+write.csv(x = large_data[1:100, 1:2000],
+          file = "data/input-temperature-small.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:100, 1:2000],
+        file = "data/temperature-small.Rds")
 
 # Medium data, 1000 x 2000; 10 times more rows than small data
-write.csv(x = large_data[1:1000, 1:2000], file = "data/input-temperature-medium.csv", row.names = FALSE)
-saveRDS(large_data[1:1000, 1:2000], file = "data/temperature-medium.Rds")
+write.csv(x = large_data[1:1000, 1:2000],
+          file = "data/input-temperature-medium.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:1000, 1:2000],
+        file = "data/temperature-medium.Rds")
 
 # Large data; 1000 x 10000; 10 times more rows, 5 times more columns than small
-write.csv(x = large_data[1:1000, 1:10000], file = "data/input-temperature-large.csv", row.names = FALSE)
-saveRDS(large_data[1:1000, 1:10000], file = "data/temperature-large.Rds")
+write.csv(x = large_data[1:1000, 1:10000],
+          file = "data/input-temperature-large.csv",
+          row.names = FALSE)
+saveRDS(large_data[1:1000, 1:10000],
+        file = "data/temperature-large.Rds")
